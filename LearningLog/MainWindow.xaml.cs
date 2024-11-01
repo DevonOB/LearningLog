30d10af66aff197303339ba38656067232854683
DevonOB <156467102+DevonOB@users.noreply.github.com>
2024-11-01 16:18:52 -0400

    Untested play recording, save "recording" and text, and delete recording

--- LearningLog/MainWindow.xaml.cs
+++ LearningLog/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System;
+using System.Media;
 
 
 namespace LearningLog
@@ -20,14 +21,62 @@ namespace LearningLog
     /// </summary>
     public partial class MainWindow : Window
     {
+        
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        string recordingInfo = string.Empty;
+
         private void buttonRecord_Click(object sender, RoutedEventArgs e)
         {
-            
+            RecordWav.StartRecording();
+            MessageBoxResult recording = MessageBox.Show("Recording in progress.\nClicking any button will end the recording.", "Recording in progress", MessageBoxButton.YesNo);
+            if (recording == MessageBoxResult.Yes)
+            {
+                FileInfo newRecordingInfo = RecordWav.EndRecording();
+                textNotes.Text = newRecordingInfo.ToString();
+                recordingInfo = newRecordingInfo.ToString();
+                buttonPlay.IsEnabled = true;
+                buttonDelete.IsEnabled = true;
+            }
+            else
+            {
+                FileInfo newRecordingInfo = RecordWav.EndRecording();
+                textNotes.Text = newRecordingInfo.ToString();
+                recordingInfo = newRecordingInfo.ToString();
+                buttonPlay.IsEnabled = true;
+                buttonDelete.IsEnabled = true;
+            }
+        }
+
+        private void buttonDelete_Click(object sender, RoutedEventArgs e, Exception fileNotFoundException)
+        {
+            try
+            {
+                File.Delete(recordingInfo);
+            }
+            catch
+            {
+                throw fileNotFoundException;           
+            }
+        }
+
+        private void buttonPlay_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer newRecording = new SoundPlayer(recordingInfo);
+            newRecording.Play();
+        }
+
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+            File.WriteAllText(Environment.CurrentDirectory + "Files\\RecordingInfo" + DateTime.Now.ToString("yyyyMMdd") + ".text", textNotes.Text);
+        }
+
+        private void textNotes_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            buttonSave.IsEnabled = true;
         }
     }
 }
