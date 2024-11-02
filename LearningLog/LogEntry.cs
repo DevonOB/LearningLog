fd6ec35d3dbe3e9894141175b7fb63bbd58e8b4a
DevonOB <156467102+DevonOB@users.noreply.github.com>
2024-11-02 16:23:36 -0400

    Mostly functional. Still needs commenting and to make tghe sumary tab working

---
+++ LearningLog/LogEntry.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningLog
+{
+    class LogEntry
+    {
+        private int logID;
+        private string logEntryDate;
+        private int logWellness;
+        private int logQuality;
+        private string logNotes;
+        private FileInfo logFile;
+
+        //LogEntry class definition
+        public LogEntry(int ID, string EntryDate, int Wellnes, int Quality, string Notes, FileInfo RecordingFile)
+        {
+            logID = ID;
+            logEntryDate = EntryDate;
+            logWellness = Wellnes;
+            logQuality = Quality;
+            logNotes = Notes;
+            logFile = RecordingFile;
+            
+        }
+
+        public int GetID()
+        {
+            return logID;
+        }
+
+        public string GetEntryDate()
+        {
+            return logEntryDate;
+        }
+
+        public int GetWellness()
+        {
+            return logWellness;
+        }
+
+        public int GetQuality()
+        {
+            return logQuality;
+        }
+
+        public string GetNotes()
+        {
+            return logNotes;
+        }
+
+        public FileInfo GetRecordingFile()
+        {
+            return logFile;
+        }
+    }
+}
--- LearningLog/MainWindow.xaml.cs
+++ LearningLog/MainWindow.xaml.cs
@@ -27,74 +27,107 @@ namespace LearningLog
             InitializeComponent();
         }
 
+        int ID = 0;
         string recordingInfo = string.Empty;
         string recordingNotes = string.Empty;
-        private readonly Exception fileNotFoundException;
+        FileInfo storedInfo;
+        bool recording = false;
 
         private void buttonRecord_Click(object sender, RoutedEventArgs e)
         {
-            //RecordWav.StartRecording();
-            MessageBoxResult recording = MessageBox.Show("Recording in progress.\nClicking any button will end the recording.", "Recording in progress", MessageBoxButton.YesNo);
-            if (recording == MessageBoxResult.Yes)
+            if (recording == false )
             {
-                FileInfo newRecordingInfo = RecordWav.EndRecording();
-                recordingInfo = newRecordingInfo.ToString();
-                buttonPlay.IsEnabled = true;
+                RecordWav.StartRecording();
+                recording = true;
+                buttonRecord.Content = ("Stop recording");
+                statusChange("New recording started");
             }
             else
             {
+                statusChange("Recording ended");
+                buttonRecord.Content = ("Record");
                 FileInfo newRecordingInfo = RecordWav.EndRecording();
                 recordingInfo = newRecordingInfo.ToString();
                 buttonPlay.IsEnabled = true;
+                recording = false;
+                buttonRecord.IsEnabled = false;
+                buttonDelete.IsEnabled = true;
             }
+            
         }
 
         private void buttonDelete_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                File.Delete(recordingInfo);
-                recordingInfo = string.Empty;
-                
+                if (recordingInfo != string.Empty)
+                {
+                    statusChange("Deleted recording");
+                    File.Delete(recordingInfo);
+                    recordingInfo = string.Empty;
+                    buttonRecord.IsEnabled = true;
+                    buttonDelete.IsEnabled = false;
+                    buttonSave.IsEnabled = false;
+                    buttonPlay.IsEnabled = false;
+                }
+                else
+                {
+                    statusChange("Failed to delete recording");
+                    throw new FileNotFoundException("No wav file availible for deletion");
+                }
             }
-            catch
+            catch (FileNotFoundException a)
             {
-                throw fileNotFoundException;           
+                MessageBox.Show(a.Message);
             }
-            try
-            {
-                File.Delete(recordingNotes);
-
-            }
-            catch
-            {
-                throw fileNotFoundException;
-            }
+            
         }
 
         private void buttonPlay_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                SoundPlayer newRecording = new SoundPlayer(recordingInfo);
-                newRecording.Play();
+                if (recordingInfo != string.Empty)
+                {
+                    statusChange("Played recording");
+                    SoundPlayer newRecording = new SoundPlayer(recordingInfo);
+                    newRecording.Play();
+                }
+                else
+                {
+                    statusChange("Failed to played recording");
+                    throw new FileNotFoundException("No wav file is availible to play");
+
+                }
             }
-            catch
+            catch (FileNotFoundException a)
             {
-                throw fileNotFoundException;
+                MessageBox.Show(a.Message);
             }
         }
 
         private void buttonSave_Click(object sender, RoutedEventArgs e)
         {
-            recordingNotes = Environment.CurrentDirectory + "Files\\RecordingNotes" + DateTime.Now.ToString("yyyyMMdd") + ".text";
-            File.WriteAllText(recordingNotes, textNotes.Text);
-            buttonDelete.IsEnabled = true;
+            ID++;
+            LogEntry newEntry;
+            newEntry = new LogEntry(ID, DateTime.Now.ToString("yyyyMMdd"), int.Parse(comboWellness.Text), int.Parse(comboQuality.Text), textNotes.Text, storedInfo);
+            buttonDelete.IsEnabled = false;
+            statusChange("Saved recording");
+            buttonRecord.IsEnabled = true;
+            buttonPlay.IsEnabled = false;
+            buttonSave.IsEnabled = false;
+            textNotes.Text = string.Empty;
         }
 
         private void textNotes_TextChanged(object sender, TextChangedEventArgs e)
         {
             buttonSave.IsEnabled = true;
+            statusChange("Edited notes");
+        }
+
+        private void statusChange(string e)
+        {
+            textStatus.Text = e + " " +DateTime.Now;
         }
     }
 }
